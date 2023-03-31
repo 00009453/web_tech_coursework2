5cd485338e01838b7c7164aaa76246464545ae25
00009453 <wiut00009453@gmail.com>
2023-03-31 10:34:06 +0500

    adding models and basic configuration

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const errorPage = require("./controllers/error");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const jobRoute = require("./routes/job");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(jobRoute);
+app.use(errorPage.getErrorPage);
+
+mongoose
+  .connect(
+    "mongodb+srv://quiz_test_app:wiut_student@cluster0.dsykjky.mongodb.net/Students"
+  )
+  .then((result) => {
+    app.listen(process.env.PORT || 4000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
